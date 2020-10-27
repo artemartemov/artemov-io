4b578735ed06ef7ea78c9aa6adb375cab17ed54d
Artem Artemov <aartemov@hearst.com>
2020-10-26 19:48:40 -0600

    moved other presentations out

--- web/src/components/image.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-function resolveImage(src) {
-  return require(`../images/${src}`);
-}
-
-export default function Image({ src, props }) {
-  return <img src={resolveImage(src)} {...props} />;
-}
